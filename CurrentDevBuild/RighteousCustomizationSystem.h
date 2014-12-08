63a831e9f9e21d3d1a8524fdc876a9dc3145cd90
MilesLitteral <Mandaloe2@gmail.com>
2014-12-08 01:30:40 -0500

    Remade Customization System

--- CurrentDevBuild/RighteousCustomizationSystem.h
+++ CurrentDevBuild/RighteousCustomizationSystem.h
@@ -1,7 +1,5 @@
 #include "RighteousReference.h"
 
-
-
 class RighteousCustomizationSystem
 {
 private:
@@ -9,103 +7,52 @@ private:
 
 public:
 
-MemoryCore MC;
-BushWall bushWall;
-GrayHound grayHound;
-
 RighteousCustomizationSystem(){};
 ~RighteousCustomizationSystem(){};
 
+MemoryCore MC;
+BushWall bushWall;
+GrayHound grayHound;
+VA VAR;
+
 void CustomVA() {
-//	int selP;
+	int selfHelp;
 
 	/*
 
 	*/
 	std::cout << "Welcome to the Hanger!" << std::endl;
+	std::cout << "1)Customize VA 2)Customize Weapons 3)Upgrade VA 4)Upgrade Weapon 5)Change Material 6)View Stats."<< std::endl;
 
-
-};
-
-int setParts()
-{
-	int nPut;
-	
-	VA GAV;
-	grayHound.makeGrayHound(true);
-	bushWall.makeBushWall(true);
-	std::string tempS = grayHound.GetName();
-	std::string tempT = bushWall.GetName();
-
-	beg:
-	std::cout << "Select a Vertical Armor" << std::endl;
-	
-	std::cin >> nPut;
-	if(nPut > 2)
+	switch(selfHelp)
 	{
-		std::cout << "Invalid Input" << std::endl;
-		goto beg;
-	}
-	else
-	{
-	switch (nPut)
-	 {
-		
 		case 1:
-			
-			//  display size and capacity of vector
-			
-			std::cout << tempS << std::endl;
-		
-			std::cout << "The Vertical Armor has: " << MC.SpawnVAShepard.size() << " parts." << std::endl;
-			std::cout << "Capacity of The Vertical Armor is: " << MC.SpawnVAShepard.capacity() << " elements." << std::endl;
-	
-			//  put some random data into the vector using at()
-			for (long index=0; index<(long)MC.SpawnVAShepard.size(); ++index)
-			{
-				
-			 MC.SpawnVAShepard.at(index)= 0;
-			}
-	
-			//  display vector contents
-			std::cout << "The VA's Limb health is(T,LA,RA,LLG,RLG): " << std::endl;
-			//MCE.MemoryPreparation(10);
-			for (long index=0; index<(long)MC.SpawnVAShepard.size(); ++index) {
-				
-				std::cout << MC.SpawnVAShepard.at(index) << " ";
-			}
-			std::cout << std::endl;
-	
-		//GAV.GA->GrayHound();
-		//bringHound();
+		//VAR.CustomizeVA();
 		break;
 
 		case 2:
-			//  display size and capacity of vector
-	std::cout << tempT << std::endl;
-	std::cout << "The Vertical Armor has: " << MC.SpawnVAShepard.size() << " parts." << std::endl;
-	std::cout << "Capacity of The Vertical Armor is: " << MC.SpawnVAShepard.capacity() << " elements." << std::endl;
-	
-	//  put some random data into the vector using at()
-	for (long index=0; index<(long)MC.SpawnVAShepard.size(); ++index)
-	{
-		 MC.SpawnVAShepard.at(index)= 0;//bushWall.Bushwall();
-	}
-	
-	//  display vector contents
-	std::cout << "VA contains the following elements:" << std::endl;
-	for (long index=0; index<(long)MC.SpawnVAShepard.size(); index++) {
-		std::cout << MC.SpawnVAShepard.at(index);
-	}
-	std::cout << std::endl << std::endl;
-			
+		//VAR.CustomizeWeapon();
 		break;
-		}
-	 }
-	getchar();
 
-	return 1;
+		case 3:
+		//VAR.UpgradeVA();
+		break;
+
+		case 4:
+		//VAR.UpgradeWeapon();
+		break;
+
+		case 5:
+		//VAR.ShiftMaterial();
+		break;
+
+		case 6:
+		//VAR.GetStats();
+		break;
 	}
+};
+
+
 
 //void UseCustomVA(){}
 
